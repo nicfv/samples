f3e0670f7e2bddeaf1f1c559be58e7f926091747
nicfv <omg.my.junkmail@gmail.com>
2020-11-01 14:44:36 -0800

    Removed xn1 from sqrt(...)

--- C Matrix Math/mathix.c
+++ C Matrix Math/mathix.c
@@ -1,10 +1,9 @@
 double sqrt(double x) {
-	double xn = 1, xn1 = 0, epsilon = 1e-15;
+	double r = 1, epsilon = 1e-15;
 	x = absf(x);
 	if(zero(x)) { return 0; }
-	while(absf(xn-xn1) > epsilon) {
-		xn1 = xn;
-		xn = (xn+x/xn)/2;
+	while(absf(x-r*r) > x*epsilon) {
+		r = (r+x/r)/2;
 	}
-	return xn;
+	return r;
 }
