d62a53bb90e2de470d66fa1508711f615944dbe3
nicfv <omg.my.junkmail@gmail.com>
2021-02-01 16:03:30 -0800

    tabs -> spaces

--- C Matrix Math/mathix.c
+++ C Matrix Math/mathix.c
@@ -1,9 +1,9 @@
 double sqrt(double x) {
-	double r = 1;
-	x = absf(x);
-	if(zero(x)) { return 0; }
-	while(absf(x-r*r) > x*EPS) {
-		r = (r+x/r)/2;
-	}
-	return r;
+    double r = 1;
+    x = absf(x);
+    if(zero(x)) { return 0; }
+    while(absf(x-r*r) > x*EPS) {
+        r = (r+x/r)/2;
+    }
+    return r;
 }
--- JS Hexagon Map/main.js
+++ JS Hexagon Map/main.js
@@ -4,19 +4,19 @@
 const G = {};
 
 const canvas = document.getElementById('c'),
-	sliderLand = document.getElementById('percentLand'),
-	sliderSize = document.getElementById('boardSize'),
-	labelLand = document.getElementById('percentLandLabel'),
-	labelSize = document.getElementById('boardSizeLabel');
+    sliderLand = document.getElementById('percentLand'),
+    sliderSize = document.getElementById('boardSize'),
+    labelLand = document.getElementById('percentLandLabel'),
+    labelSize = document.getElementById('boardSizeLabel');
 
 G.percentLand = 0.5;
 G.boardSize = 3;
 
 const updateUi = () => {
-	G.percentLand = +sliderLand.value/100;
-	labelLand.innerText = sliderLand.value+'% land';
-	G.boardSize = +sliderSize.value;
-	labelSize.innerText = 'board size '+sliderSize.value;
+    G.percentLand = +sliderLand.value/100;
+    labelLand.innerText = sliderLand.value+'% land';
+    G.boardSize = +sliderSize.value;
+    labelSize.innerText = 'board size '+sliderSize.value;
 }
 
 const draw = () => {
